48b919f8cfff6dc9f0673210847ca926306cbfe3
dmswldkel <dmswldkel@naver.com>
2021-05-07 00:40:07 +0900

    주문관리 Order UI 변경
    이미지에서 버튼으로 UI 변경

---
+++ Design_Pattern_Project/src/main/java/DPP_Cal/Card.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DPP_Cal;
+
+/**
+ *
+ * @author USER
+ */
+public class Card extends Payment { // Card 클래스
+    @Override
+    public void pay() {
+    }
+}
---
+++ Design_Pattern_Project/src/main/java/DPP_Cal/CardStrategy.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DPP_Cal;
+
+/**
+ *
+ * @author USER
+ */
+public class CardStrategy implements PaymentStrategy { // CardStrategy 구현체
+    @Override
+    public void pay() {
+    }
+}
